2263c654701bcd727993d78612aeb6c60602b2f8
ethiqque <danielpyld.work@gmail.com>
2024-02-17 17:11:39 +0200

    Realised registration a new user

--- FirstSecurityApp/src/main/java/com/ethiqque/springcourse/FirstSecurityApp/controllers/AuthController.java
+++ FirstSecurityApp/src/main/java/com/ethiqque/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -1,15 +1,50 @@
 package com.ethiqque.springcourse.FirstSecurityApp.controllers;
 
+import com.ethiqque.springcourse.FirstSecurityApp.models.Person;
+import com.ethiqque.springcourse.FirstSecurityApp.service.RegistrationService;
+import com.ethiqque.springcourse.FirstSecurityApp.util.PersonValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    private final RegistrationService registrationService;
+    private final PersonValidator personValidator;
+
+    public AuthController(RegistrationService registrationService, PersonValidator personValidator) {
+        this.registrationService = registrationService;
+        this.personValidator = personValidator;
+    }
 
     @GetMapping("/login")
     public String loginPage(){
         return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("person")Person person){
+        return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("person") @Valid Person person,
+                        BindingResult bindingResult){
+        personValidator.validate(person, bindingResult);
+
+        if(bindingResult.hasErrors())
+            return "/auth/registration";
+
+
+        registrationService.register(person);
+
+        return "redirect:/auth/login";
+
     }
 }
---
+++ FirstSecurityApp/src/main/java/com/ethiqque/springcourse/FirstSecurityApp/util/PersonValidator.java
@@ -0,0 +1,39 @@
+package com.ethiqque.springcourse.FirstSecurityApp.util;
+
+import com.ethiqque.springcourse.FirstSecurityApp.models.Person;
+import com.ethiqque.springcourse.FirstSecurityApp.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public PersonValidator(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+
+        try {
+            personDetailsService.loadUserByUsername(person.getUsername());
+        } catch (UsernameNotFoundException ignored) {
+            return;
+        }
+
+        errors.rejectValue("username", "", "Person with this name already exist");
+
+    }
+}
