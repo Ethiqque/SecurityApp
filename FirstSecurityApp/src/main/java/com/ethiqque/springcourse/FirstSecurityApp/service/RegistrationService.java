f7a384a023b88859b34e81522375a982f7d7ba92
ethiqque <danielpyld.work@gmail.com>
2024-02-17 18:20:45 +0200

    Added password encryption

--- FirstSecurityApp/src/main/java/com/ethiqque/springcourse/FirstSecurityApp/service/RegistrationService.java
+++ FirstSecurityApp/src/main/java/com/ethiqque/springcourse/FirstSecurityApp/service/RegistrationService.java
@@ -3,6 +3,7 @@ package com.ethiqque.springcourse.FirstSecurityApp.service;
 import com.ethiqque.springcourse.FirstSecurityApp.models.Person;
 import com.ethiqque.springcourse.FirstSecurityApp.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person){
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
